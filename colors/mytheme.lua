ff87043f91d1eef096b6471adccf2ca4144db5be
MicoWata <michael.gagnon1987@gmail.com>
2024-12-16 11:37:16 -0500

    plugins

--- colors/mytheme.lua
+++ colors/mytheme.lua
@@ -3,4 +3,4 @@ vim.o.termguicolors = true
 
 -- Load the colorscheme
 local theme = require("mytheme")
-theme.load() -- we'll create this new function instead of setup()
+-- theme.load() -- we'll create this new function instead of setup()
