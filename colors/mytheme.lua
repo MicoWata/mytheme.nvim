e0550a6d4e8cdc840c80c7f7203ffba197a1740f
MicoWata <michael.gagnon1987@gmail.com>
2024-12-16 10:14:33 -0500

    Add colorscheme loader file

--- colors/mytheme.lua
+++ colors/mytheme.lua
@@ -1,2 +1,14 @@
 -- colors/mytheme.lua
+if vim == nil then
+	return
+end
+
+vim.cmd("highlight clear")
+if vim.fn.exists("syntax_on") then
+	vim.cmd("syntax reset")
+end
+
+vim.o.termguicolors = true
+vim.g.colors_name = "mytheme"
+
 require("mytheme").setup()
